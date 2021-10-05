2476b3e098207785072d6a4f11493ce970bb0ccd
jeandtx <jean.doutriaux@gmail.com>
2021-10-05 11:08:12 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    printf("Hello Clement et Tristan ca dit quoi\n");
+    return 0;
+}
+
+// PATH to run 
+// cd "/Users/jeandtx/Documents/CODE/L2/S1/Pastry-Project/" && gcc main.c -o main && "/Users/jeandtx/Documents/CODE/L2/S1/Pastry-Project/"main
